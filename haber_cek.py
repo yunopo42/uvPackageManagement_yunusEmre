804541d5567c9b7480f260f09a16e26eb0266fcf
yunopo42 <yunusemreatmaz@gmail.com>
2026-07-22 13:39:18 +0300

    Uv succesfully installed to system and was worked in venv

---
+++ haber_cek.py
@@ -0,0 +1,17 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests>=2.34.2",
+# ]
+# ///
+
+
+def main() -> None:
+    print("Hello from haber_cek.py!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+# bu dosyayı birine gönderdiğinde uv run haber_cek.py ile gerekli kütüphaneleri de otomatik indirmiş şekilde çalıştırır.
